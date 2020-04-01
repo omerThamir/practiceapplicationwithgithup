f24f76cef20335fd6c1c4a5a1332e078c2c0f934
omar <omar.thamer.cs@gmail.com>
2020-04-01 19:04:04 +0300

    adding text view and one button and using on click listener

--- app/src/main/java/com/omar/myapps/practiceapplicationwithgithup/MainActivity.java
+++ app/src/main/java/com/omar/myapps/practiceapplicationwithgithup/MainActivity.java
@@ -1,21 +1,35 @@
 package com.omar.myapps.practiceapplicationwithgithup;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private void showMessage() {
-        Toast.makeText(this, "fuccking line added", Toast.LENGTH_SHORT).show();
-    }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "hello home!!!!", Toast.LENGTH_SHORT).show();
+            textView.setText("message was shown");
+        }
+    };
+
+    private Button showMessageBtn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        showMessage();
+
+        showMessageBtn = findViewById(R.id.shoeMessagebutton);
+        textView = findViewById(R.id.textview);
+        showMessageBtn.setOnClickListener(onClickListener);
+
     }
 }
